1772028b1b1935e1b24abb80ea722426a9221434
vishnuvigneshy <89254315+vishnuvigneshy@users.noreply.github.com>
2021-08-20 17:33:00 +0530

    Add files via upload

---
+++ central bank of tamilnadu/footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© 2021. Made by vishnuvignesh <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ central bank of tamilnadu/index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>CENTRAL BANK OF TAMILNADU</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">CUSTOMERS</button></a></li>
+        <li class="operations"><a href="send.php"><button class="btn" id="blue">SEND</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">TRANSACTION HISTORY</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
